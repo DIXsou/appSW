7e3ab2c49d2e3d7132b21304ad817328019bea84
Diego <DIXsou@gmail.com>
2018-02-02 20:55:14 +0100

    el circulo inicial siempre esta centrado. //LO LOGREEE

--- javascrip/index.js
+++ javascrip/index.js
@@ -1,5 +1,13 @@
 window.onload = function(){
-    var puntoIntermedioY = window.innerHeight / 2;
+    var rz = null;
+    window.onresize = function(){
+        clearTimeout(rz);
+        rz = setTimeout(resizing, 100);
+    };
+    resizing();
+    
+    function resizing(){
+        var puntoIntermedioY = window.innerHeight / 2;
     var puntoIntermedioX = window.innerWidth / 2;
     var dimenciones
     if(puntoIntermedioX >= puntoIntermedioY){
@@ -8,12 +16,14 @@ window.onload = function(){
     else{
         dimenciones = puntoIntermedioX;
     }
-    
     var divMenu = document.getElementById("menu");
-    divMenu.getChild.style.setProperty("width",dimenciones+"px");
-    divMenu.style.setProperty("height",dimenciones+"px");
+    var imgMenu = document.getElementById("imgMenu");
+    imgMenu.style.setProperty("width",dimenciones+"px");
+    imgMenu.style.setProperty("height",dimenciones+"px");
     divMenu.style.setProperty("left",(puntoIntermedioX-(dimenciones/2))+"px");
     divMenu.style.setProperty("top",(puntoIntermedioY-(dimenciones/2))+"px");
+    }
+    
 
 
     var divOpcion = document.createElement("div").setAttribute("id","opcion");
