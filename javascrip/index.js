07582275f301c92a9ddd5cf66f775ed30ab3d242
Diego <DIXsou@gmail.com>
2018-02-22 21:13:53 +0100

    empezar lo de ajax

--- javascrip/index.js
+++ javascrip/index.js
@@ -6,6 +6,29 @@ window.onload = function(){
     }
 }
 
+function descargaArchivo() {
+    // Obtener la instancia del objeto XMLHttpRequest
+    if(window.XMLHttpRequest) {
+        peticion_http = new XMLHttpRequest();
+    }
+    else if(window.ActiveXObject) {
+        peticion_http = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    // Preparar la funcion de respuesta
+    peticion_http.onreadystatechange = muestraContenido;
+    // Realizar peticion HTTP
+    peticion_http.open('GET', 'marcas.xml', true);
+    peticion_http.send(null);
+    function muestraContenido() {
+        if(peticion_http.readyState == 4) {
+            if(peticion_http.status == 200) {
+                recorrerFichero(peticion_http.responseText);
+            }
+        }				
+    }
+}
+
+
 var opciones = [,,,,,];
 var rz = null;
 window.onresize = function(){
